b842f175d818c879fd52f3fcd7d501aca54ba3d1
Daniel Mason <danmasson0@gmail.com>
2022-07-19 21:09:25 -0400

    Updates

---
+++ precache-manifest.1188f1d0cd32da362759034c10b82c9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7abbf76fd2172565f45de95685dc6a5",
+    "url": "/navbar-react-hook-practise/index.html"
+  },
+  {
+    "revision": "4a8a01eb43d55948cbf4",
+    "url": "/navbar-react-hook-practise/static/css/main.c35e1b0f.chunk.css"
+  },
+  {
+    "revision": "5036664f6a0cc62b8b04",
+    "url": "/navbar-react-hook-practise/static/js/2.41d20619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/navbar-react-hook-practise/static/js/2.41d20619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8a01eb43d55948cbf4",
+    "url": "/navbar-react-hook-practise/static/js/main.4d6e3d3e.chunk.js"
+  },
+  {
+    "revision": "ccc520d7c9e7e6f92693",
+    "url": "/navbar-react-hook-practise/static/js/runtime-main.fe14302a.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/navbar-react-hook-practise/static/media/logo.2bb7da65.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/navbar-react-hook-practise/precache-manifest.1188f1d0cd32da362759034c10b82c9b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/navbar-react-hook-practise/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
